b857ae1e7bdc37c54d10ecbf68c91501eafddfd2
alistkov <antonlistkov@gmail.com>
2021-11-22 23:23:11 +0300

    finish brain-even game

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,8 +2,10 @@ import prompt
 
 
 def welcome_user():
+    print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print('Hello, {}!'.format(name))
+    return name
 
 
 def main():
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import prompt
+import random
+from brain_games.cli import welcome_user
+
+
+def is_even(num):
+    return num % 2 == 0
+
+
+def brain_even():
+    name = welcome_user()
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    steps = 3
+    while steps > 0:
+        number = random.randint(1, 100)
+        print(f'Question: {number}')
+        user_answer = prompt.string('Answer: ')
+        correct_answer = 'yes' if is_even(number) else 'no'
+        if user_answer.lower() != correct_answer:
+            print(
+                f'\'{user_answer}\' is wrong ' +
+                f'answer ;(. Correct answer was \'{correct_answer}\''
+                )
+            print(f'Let\'s try again, {name}!')
+            return
+        print('Correct!')
+        steps -= 1
+    print(f'Congratulations, {name}!')
+
+
+def main():
+    brain_even()
+
+
+if __name__ == '__main__':
+    main()
